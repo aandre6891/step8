303023c61407a098116c64e31be2adf3e2044a09
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-05 10:34:17 +0100

    add check typos when writing cohort

--- directory_ex7.rb
+++ directory_ex7.rb
@@ -1,3 +1,21 @@
+def check_cohort
+  months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+  # get the cohort
+  puts "Please, enter the cohort of this student"
+  @cohort = gets.chomp.downcase
+  if @cohort.empty? 
+    @cohort = "2023"
+  elsif !@cohort.empty? && !months.include?(@cohort)
+    loop do
+      puts "wrong month, write it better"
+      @cohort = gets.chomp.downcase
+      if months.include?(@cohort)
+        break
+      end
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -6,27 +24,18 @@ def input_students
   # get the first name
   name = gets.chomp.upcase
   if !name.empty?
-    # get the cohort
-    puts "Please, enter the cohort of this student"
-    cohort = gets.chomp.downcase
-    if cohort.empty? 
-      cohort = "2023"
-    end
+    check_cohort
   end
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym}
+    students << {name: name, cohort: @cohort.to_sym}
     puts "Now we have #{students.count} students, add a new one or hit return to stop"
     # get another name from the user
     name = gets.chomp.upcase
     if !name.empty?
       # get the cohort of this student
-      puts "Please, enter the cohort of this student"
-      cohort = gets.chomp.downcase
-      if cohort.empty? 
-        cohort = "2023"
-      end
+      check_cohort
     end
   end
   # return the array of students
